6be15a8dbdd816e6ce1d3dca433583322500d9ce
Dom Rollo <dfrollo@gmail.com>
2021-08-31 11:08:07 -0400

    Removing some junk from tracking

--- ctello/Tello/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lSDL2 -lboost_thread -lboost_system -lopencv_videoio -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs
-
